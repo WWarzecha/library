2810291fe9d649fb23a320d5c89ab041d8e7260b
Woodenspoon <wmwarzecha@gmail.com>
2024-12-30 13:57:54 +0100

    feat: add books styles

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function Book(title, author, pages, isRead){
+*,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+
+function Book(title = "title unknown", author = "author unknown", pages = "unknown number of ", isRead = false){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = () => `${this.title} by ${this.author}, ${pages} pages, ${(this.isRead) ? "already read" : "not read yet"}`;
 };
+
+Book.prototype.info = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.isRead) ? "already read" : "not read yet"}`;
+}
+
+let book = new Book();
+console.log(book.info());
